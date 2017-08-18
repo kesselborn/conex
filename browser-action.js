6e8fb8099798f1795f48f310b77458947cc088c5
Daniel Bornkessel <daniel@bornkessel.com>
2017-08-18 18:22:28 +0200

    Cleanup code

--- browser-action.js
+++ browser-action.js
@@ -25,7 +25,7 @@ const keyHandling = function(event) {
       searchElement.value = event.key;
     }
   }
-}
+};
 
 document.body.addEventListener('keypress', keyHandling);
 
@@ -42,62 +42,81 @@ const addClickListenerToTabs = function(tabGroups) {
   }
 };
 
+const resetSearch = function() {
+  document.getElementById('history').innerHTML = '';
+  for(ul of $('#tabgroups ul')) {
+    ul.style.display = '';
+    ul.querySelector('li.section').tabIndex = 1;
+  }
+
+  for(li of $('#tabgroups li.thumbnail')) {
+    li.style.display = 'none';
+  }
+};
+
+const fillHistorySection = function(searchQuery) {
+  const historyUl = $1('#history');
+
+  historyUl.innerHTML = '';
+  historyUl.appendChild(sectionElement('', 'none', 'history', -1));
+
+  browser.history.search({
+    text: searchQuery,
+    startTime: 0
+  }).then(result => {
+    const tabLinks = Array.from($('.thumbnail')).map(t => t.dataset.url.toLowerCase());
+
+    result
+      .sort((a,b) => b.visitCount - a.visitCount)
+      .filter(e => ! tabLinks.includes(e.url.replace('http://','').replace('https://','').toLowerCase()))
+      .forEach(searchResult => historyUl.appendChild(makeHistoryItem(searchResult)));
+  }, e => console.error(e));
+};
+
+const showHideTabEntries = function(searchQuery) {
+  for(element of $('.thumbnail')) {
+    const text = (element.dataset.title + element.dataset.url.replace('http://','').replace('https://')).toLowerCase();
+    if(text) {
+      // if the search query consists of multiple work, we search for a match of any of the words
+      const match = searchQuery.split(' ').every(q => {
+        return text.indexOf(q) >= 0
+      });
+      element.style.display = match ? '' : 'none';
+    }
+  }
+};
+
+const showHideGroupEntries = function(searchQuery) {
+  for(ul of $('#tabgroups ul')) {
+    ul.querySelector('li.section').tabIndex = -1; // section should not be selectable when we have search results
+
+    // hide sections that don't have tabs that match the search
+    if(Array.from(ul.querySelectorAll('li.thumbnail')).filter(li => li.style.display != 'none') == 0) {
+      ul.style.display = 'none';
+    } else {
+      ul.style.display = '';
+    }
+  }
+};
+
 // TODO: refactor
 const onSearchChange = function(event) {
-  if(event.target.value != '') {
-    Array.from(document.querySelectorAll('.thumbnail')).forEach(function(element) {
-      let searchTerms = element.dataset.title + element.dataset.url.replace('http://','').replace('https://');
-      if(searchTerms) {
-        let matchesSearchTerms = event.target.value.split(' ').every(searchTerm => {
-          return searchTerms.indexOf(searchTerm.toLowerCase()) >= 0
-        });
-        element.style.display = matchesSearchTerms ? '' : 'none';
-      }
-    });
-
-    Array.from($('#tabgroups ul')).forEach(ul => {
-      ul.querySelector('li.section').tabIndex = -1; // section should not be selectable when we have search results
-
-      // hide sections that don't have tabs that match the search
-      if(Array.from(ul.querySelectorAll('li.thumbnail')).filter(li => li.style.display != 'none') == 0) {
-        ul.style.display = 'none';
-      } else {
-        ul.style.display = '';
-      }
-    });
-
-    // search history for search strings that are longer than one letter
-    if(event.target.value.length > 1 && $('#history ul li').length == 0) {
-      console.log('fetching history');
-      document.getElementById('history').innerHTML = '';
-      let historyUl = document.getElementById('history');
-      let historySection = sectionElement('', 'none', 'history', -1);
-      historyUl.appendChild(historySection);
-      var searching = browser.history.search({
-        text: event.target.value,
-        startTime: 0
-      }).then(result => {
-        let tabLinks = Array.from($('#tabgroups li')).map(t => t.dataset.url);
-        let historyTags = result
-          .sort((a,b) => b.visitCount - a.visitCount)
-          .filter(e => ! tabLinks.includes(e.url.replace('http://','').replace('https://','').toLowerCase()))
-          .forEach(searchResult => historyUl.appendChild(makeHistoryItem(searchResult)));
-      }, e => console.error(e));
-    } else if(event.target.value.length <= 1) {
-      console.log('deleting history');
-      document.getElementById('history').innerHTML = '';
-    } else {
-      console.log('using cached history');
-    }
-  } else {
-    document.getElementById('history').innerHTML = '';
-    Array.from($('#tabgroups ul')).forEach(ul => {
-      ul.style.display = '';
-      ul.querySelector('li.section').tabIndex = 1;
-    });
-    Array.from($('#tabgroups li.thumbnail')).forEach(li => li.style.display = 'none' )
+  const searchQuery = event.target.value.toLowerCase();
+  if(searchQuery == '') {
+    return resetSearch();
   }
-}
+
+  showHideTabEntries(searchQuery);
+  showHideGroupEntries(searchQuery);
+
+  if(searchQuery.length > 1 && $('#history ul li').length == 0) {
+    console.log('fetching history');
+    fillHistorySection(searchQuery);
+  } else if(searchQuery.length <= 1) {
+    console.log('deleting history');
+    $1('#history').innerHTML = '';
+  }
+};
 
 setTimeout(function(){
   document.getElementById('search').focus();
