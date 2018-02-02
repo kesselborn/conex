45df43596a804b1c4b5fdc432818582fcb4994ae
kesselborn <daniel@bornkessel.com>
2018-02-02 21:29:45 +0100

    Sort container names in list -- make default always first
    fixes kesselborn/conex#137

--- conex-actions-common.js
+++ conex-actions-common.js
@@ -2,8 +2,9 @@ const renderTabContainers = async function(parent) {
   const identities = await browser.contextualIdentities.query({});
 
   browser.browserAction.setBadgeText({text: ''});
-  for(const context of identities.concat({cookieStoreId: 'firefox-default', color: 'default', name: 'default'})
-                                 .concat({cookieStoreId: 'firefox-private', color: 'private', name: 'private browsing tabs'})) {
+  for(const context of [{cookieStoreId: 'firefox-default', color: 'default', name: 'default'}]
+                       .concat(identities.sort((a,b) => a.name.toLowerCase() > b.name.toLowerCase()))
+                       .concat({cookieStoreId: 'firefox-private', color: 'private', name: 'private browsing tabs'})) {
     parent.appendChild(createTabContainerHeaderElement(context.cookieStoreId, context.color, context.name));
   }
 
