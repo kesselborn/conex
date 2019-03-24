5aec94df543df12566da24b673012b935eb7b3da
Daniel Bornkessel <daniel@bornkessel.com>
2019-03-24 20:52:20 +0100

    Focus first element of sidebar

--- conex-sidebar.js
+++ conex-sidebar.js
@@ -1,10 +1,18 @@
 import "./conex-tab-component.js";
 import "./conex-container-component.js";
+import {$1} from "./conex-helper.js";
 
 const bg = window.browser.extension.getBackgroundPage();
 
 document.addEventListener("DOMContentLoaded", () => {
-  bg.initializingConex.then(getDomTree => document.body.firstElementChild.replaceWith(getDomTree()), e => console.error(e));
+  bg.initializingConex.then(
+    getDomTree => {
+      document.body.firstElementChild.replaceWith(getDomTree());
+      $1("container-item").focus();
+    },
+    e => console.error(e)
+  );
+
 });
 
 console.debug("conex-sidebar.js loaded");
