dedee29598bf56cba6a28c4687203ad4ca49eafa
Daniel Bornkessel <conex@bornkessel.com>
2021-08-18 23:17:10 +0200

    Show debug on options ui when debug param is given

--- conex-options-ui.js
+++ conex-options-ui.js
@@ -1,15 +1,14 @@
-import { $e, logLevel, debug } from './conex-helper.js';
+import { $ } from './conex-helper.js';
 
 const params = new URLSearchParams(window.location.search);
 
 if (params.get('debug')) {
-  document.body.appendChild(
-    $e('a', { href: browser.runtime.getURL('/test/conex-test.html'), content: 'run test suite' })
-  );
+  $('section#debug').style.display = 'block';
 }
 
-if (params.get('log')) {
-  logLevel(params.get('log'));
-}
+// if (params.get('log')) {
+//   logLevel(params.get('log'));
+// }
 
-debug('console.debug output');
+console.log('boom');
+// debug('console.debug output');
