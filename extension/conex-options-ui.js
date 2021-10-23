67f2d786deb9469f5ea89762c6ac88b6a97c347d
Daniel Bornkessel <conex@bornkessel.com>
2021-10-23 14:14:04 +0200

    Lint before commit

--- extension/conex-options-ui.js
+++ extension/conex-options-ui.js
@@ -1,23 +1,23 @@
 import { $ } from './conex-helper.js';
 async function showHideDebugUI() {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get('debug') === '1') {
-        await browser.storage.local.set({
-            showDebugUI: true,
-        });
-    }
-    if (params.get('debug') === '0') {
-        await browser.storage.local.set({
-            showDebugUI: false,
-        });
-    }
-    if ((await browser.storage.local.get('showDebugUI'))['showDebugUI'] === true) {
-        $('section#debug').style.display = 'block';
-    }
-    // if (params.get('log')) {
-    //   logLevel(params.get('log'));
-    // }
-    console.log('boom');
-    // debug('console.debug output');
+  const params = new URLSearchParams(window.location.search);
+  if (params.get('debug') === '1') {
+    await browser.storage.local.set({
+      showDebugUI: true,
+    });
+  }
+  if (params.get('debug') === '0') {
+    await browser.storage.local.set({
+      showDebugUI: false,
+    });
+  }
+  if ((await browser.storage.local.get('showDebugUI')).showDebugUI === true) {
+    $('section#debug').style.display = 'block';
+  }
+  // if (params.get('log')) {
+  //   logLevel(params.get('log'));
+  // }
+  console.log('boom');
+  // debug('console.debug output');
 }
 showHideDebugUI();
